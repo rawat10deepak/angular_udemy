d3fbf59fa69b48eabe7cd2bd52f015ed1b070c46
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-02 14:08:46 +0530

    2 files modified

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]', // select by attribute
+  // selector: '.app-servers', // select by class
   template: `
       <app-server></app-server>
       <app-server></app-server>`,
