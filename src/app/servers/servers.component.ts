a6596555c3762b7186bfce74639c7a479136c0d9
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-02 13:55:12 +0530

    3 files modified

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,7 +5,12 @@ import { Component, OnInit } from '@angular/core';
   template: `
       <app-server></app-server>
       <app-server></app-server>`,
-  styleUrls: ['./servers.component.css'],
+  // styleUrls: ['./servers.component.css'],
+  styles: [
+    `h3 {
+    color: dodgerblue;
+  }`,
+  ],
 })
 export class ServersComponent implements OnInit {
   constructor() {}
